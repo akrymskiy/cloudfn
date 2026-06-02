b39da7726fd7a75aee3d8c4ac55ca42de0a6997e
Aleksandr Krymskiy <alex@krymskiy.net>
2026-06-01 22:39:50 -0400

    Release cloudfn-core==0.2.7

--- cloudfn/core/cli.py
+++ cloudfn/core/cli.py
@@ -6,12 +6,32 @@ from importlib import import_module
 from importlib.util import spec_from_file_location, module_from_spec
 from inspect import getmembers
 from pathlib import Path
-from sys import argv
+import sys
 from traceback import extract_tb
+
+from cloudfn.core.common import FNPaths, get_fn_paths, print_e
+
+def import_cfn_module(module_name: str):
+	"""Import cfn module dynamically"""
+	try:
+		return import_module(module_name)
+
+	except ModuleNotFoundError as mnfe:
+		if mnfe.name.startswith('cloudfn.aws'):
+			print_e('Required `cloudfn-aws` package is not installed.')
+		elif mnfe.name.startswith('cloudfn.azure'):
+			print_e('Required `cloudfn-azure` package is not installed.')
+		elif mnfe.name.startswith('cloudfn.gcp'):
+			print_e('Required `cloudfn-gcp` package is not installed.')
+		else:
+			print_e(f'Please install package containing module `{mnfe.name}`')
+
+		sys.exit(1)
 
 def deploy_fn(args):
 	"""Deploy function"""
 
+	fn_paths = get_fn_paths(args.path)
 	fn_path = Path(args.fn_path)
 
 	# Check file
@@ -108,34 +128,85 @@ def deploy_fn(args):
 
 	return 0
 
-def deploy_layer(args):
-	"""Deploy layer"""
-	function_path = Path(args.function_path)
-	if function_path.is_file():
-		print(f'Attempting to load: {args.function_path}')
-	else:
-		print(f'{function_path.absolute()} is not a file')
-		return 1
+def deploy_aws_layer(args):
+	"""Deploy AWS function layer"""
+	print('CFN AWS Layer Deploy')
+	print('--------------------')
+	fn_paths = get_fn_paths(args.path)
+
+	if not fn_paths:
+		print_e(f'Path `{args.path}` does not exists')
+		sys.exit(1)
+
+	if not fn_paths.requirements_path:
+		print_e(f'`requirements.txt` not found in the project folder')
+		sys.exit(1)
+
+	deploy_module = import_cfn_module('cloudfn.aws.deploy')
+	deploy_module.deploy_fn_layer(fn_paths, args.aws_profile, args.aws_region, args.layer_builder_function)
+
+	return 0
+
+def deploy_aws_fn(args):
+	"""Deploy AWS function"""
+	print('CFN AWS Function Deploy')
+	print('-----------------------')
+
+	fn_paths = get_fn_paths(args.path)
+
+	if not fn_paths:
+		print_e(f'Path `{args.path}` does not exists')
+		sys.exit(1)
+
+	deploy_module = import_cfn_module('cloudfn.aws.deploy')
+	deploy_module.deploy_fn(fn_paths, args.aws_profile, args.aws_region)
+
+	return 0
+
+def deploy_aws_layer_builder_fn(args):
+	"""Create AWS LayerBuilder function"""
+
+	print('CFN AWS Lambda Builder Function Deploy')
+	print('-----------------------')
+
+	deploy_module = import_cfn_module('cloudfn.aws.deploy')
+	deploy_module.deploy_layer_builder_fn(args.layer_builder_function, args.aws_profile, args.aws_region)
 
 	return 0
 
 def main():
 	"""CLI Entry-Point"""
 	# Process arguments
-	arg_parser = ArgumentParser(description='cloudfn cli')
+	arg_parser = ArgumentParser(description='cloudfn CLI')
 	subparsers = arg_parser.add_subparsers(help='sub-command help', dest='cmd', required=True)
-	#subparsers.add_argument('action', choices=['deploy'])
 
-	deploy_fn_parser = subparsers.add_parser('deploy-fn', aliases=['df'], help='deploys function')
-	deploy_fn_parser.add_argument('--fn-path', '-fp', required=True, help='path help')
-	deploy_fn_parser.set_defaults(function=deploy_fn)
+	deploy_aws_fn_parser = subparsers.add_parser('deploy-aws-fn', help='deploys function to AWS Lambda')
+	deploy_aws_fn_parser.add_argument('--path', required=True, help='path to function')
+	# deploy_aws_fn_parser.add_argument('--aws-profile', required=False, help='AWS CLI profile name')
+	# deploy_aws_fn_parser.add_argument('--aws-region', required=False, help='AWS region')
+	deploy_aws_fn_parser.set_defaults(function=deploy_aws_fn)
 
-	deploy_layer_parser = subparsers.add_parser('deploy-layer', aliases=['dl'], help='deploys layer')
-	deploy_layer_parser.add_argument('--fn-path', '-fp', help='path help')
-	deploy_layer_parser.set_defaults(function=deploy_layer)
+	deploy_aws_layer_parser = subparsers.add_parser('deploy-aws-layer', help='deploys layer to AWS Lambda')
+	deploy_aws_layer_parser.add_argument('--path', required=True, help='path to function or to project folder')
+	# deploy_aws_layer_parser.add_argument('--aws-profile', required=False, help='AWS CLI profile name')
+	# deploy_aws_layer_parser.add_argument('--aws-region', required=False, help='AWS region')
+	deploy_aws_layer_parser.add_argument('--layer-builder-function', required=False, default='CFN_LambdaLayerBuilder', help='name or ARN of the deploy function')
+	deploy_aws_layer_parser.set_defaults(function=deploy_aws_layer)
 
-	args = arg_parser.parse_args(argv[1:])
-	# print(args)
+	deploy_aws_layer_builder_fn_parser = subparsers.add_parser('create-aws-layer-builder-fn', help='deploys LayerBuilder helper function')
+	deploy_aws_layer_builder_fn_parser.add_argument('--layer-builder-function', required=False, default='CFN_LambdaLayerBuilder', help='name for the deploy function')
+	# deploy_aws_layer_builder_fn_parser.add_argument('--aws-profile', required=False, help='AWS CLI profile name')
+	# deploy_aws_layer_builder_fn_parser.add_argument('--aws-region', required=False, help='AWS region')
+	deploy_aws_layer_builder_fn_parser.set_defaults(function=deploy_aws_layer_builder_fn)
+
+	# Common AWS parser args
+	for aws_sub_parser in (deploy_aws_fn_parser, deploy_aws_layer_parser, deploy_aws_layer_builder_fn_parser):
+		aws_sub_parser.add_argument('--aws-profile', required=False, help='AWS CLI profile name')
+		aws_sub_parser.add_argument('--aws-region', required=False, help='AWS region')
+
+
+	args = arg_parser.parse_args(sys.argv[1:])
+
 	return args.function(args)
 
 	# print(args)
@@ -154,8 +225,13 @@ def main():
 if __name__ == '__main__':
 	import unittest.mock
 	with unittest.mock.patch(
-		'__main__.argv',
+		'__main__.sys.argv',
 		# r'cfn df -fp C:\Users\avk\Repos\CF\cf-dw-lambda\Cartesian\APICollector.py'.split()):
-		r'cfn deploy-fn --fn-path C:\Users\avk\Repos\Personal\cloudfn\example\Example.py'.split()):
+		# r'cfn deploy-aws-fn --fn-path C:\Users\avk\Repos\Personal\cloudfn\example\Example.py'.split()):
+		# r'cfn deploy-aws-layer --fn-path C:\Users\avk\Repos\Personal\cloudfn\example\Example.py'.split()):
+		r'cfn create-aws-layer-builder-fn --aws-profile ba-prod'.split()):
 		print('Ret Val = ', main())
+
+
+	# cfn deploy-layer --fn-path C:\Users\avk\Repos\DM\dm-lambda\Athena\QueryAcceptor.py
 # endregion
